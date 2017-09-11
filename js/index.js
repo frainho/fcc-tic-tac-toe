ad879d4533d131ca41bf0f7eae773a6861eaf352
Filipe Rainho <frainho@Filipes-MacBook-Pro.local>
2017-09-12 01:37:54 +0200

    finished computer move engine

--- js/index.js
+++ js/index.js
@@ -1,19 +1,40 @@
 $( document ).ready(function() {
   var playerOps = ["O", "X"];
-  var coord = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var coord = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
   
 
   $('td').click( function(){
     $(this).text("X");
-    var idInTable = $(this).attr('id');
-    console.log(idInTable);
-    var removeNum = coord.indexOf(idInTable);
+    let idInTable = $(this).attr('id');
+    let removeNum = coord.indexOf(idInTable);
     console.log(removeNum);
     if (removeNum != -1) {
-      array.splice(removeNum, 1);
+      coord.splice(removeNum, 1);
     }
     console.log(coord);
-  }); 
+    if (coord.length == 0) {
+      console.log('Game finished');
+      return;
+    }
+    computerMove();
+  });
+  
   function computerMove() {
+  let computerPlay = coord[Math.floor(Math.random()*coord.length)];
+  $('#' + computerPlay).text("O");
+  let idInTable = $('#' + computerPlay).attr('id');
+  let removeNum = coord.indexOf(idInTable);
+  console.log(removeNum);
+  if (removeNum != -1) {
+    coord.splice(removeNum, 1);
+  }
+  if (coord.length == 0) {
+    console.log('Game finished');
+    return;
+  }
+  }
+
+  if (coord.length == 0) {
+    console.log('Game finished');
   }
 });
