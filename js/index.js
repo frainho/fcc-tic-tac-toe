8f8458c094c1c298f3b8dfc83c2a9d540d6b9842
Filipe Neves <filipenrainho@gmail.com>
2017-09-12 01:47:59 +0200

    updated indentation

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-$( document ).ready(function() {
+$(document).ready(function () {
   var playerOps = ["O", "X"];
   var coord = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  
 
-  $('td').click( function(){
+
+  $('td').click(function () {
     $(this).text("X");
     let idInTable = $(this).attr('id');
     let removeNum = coord.indexOf(idInTable);
@@ -18,20 +18,20 @@ $( document ).ready(function() {
     }
     computerMove();
   });
-  
+
   function computerMove() {
-  let computerPlay = coord[Math.floor(Math.random()*coord.length)];
-  $('#' + computerPlay).text("O");
-  let idInTable = $('#' + computerPlay).attr('id');
-  let removeNum = coord.indexOf(idInTable);
-  console.log(removeNum);
-  if (removeNum != -1) {
-    coord.splice(removeNum, 1);
-  }
-  if (coord.length == 0) {
-    console.log('Game finished');
-    return;
-  }
+    let computerPlay = coord[Math.floor(Math.random() * coord.length)];
+    $('#' + computerPlay).text("O");
+    let idInTable = $('#' + computerPlay).attr('id');
+    let removeNum = coord.indexOf(idInTable);
+    console.log(removeNum);
+    if (removeNum != -1) {
+      coord.splice(removeNum, 1);
+    }
+    if (coord.length == 0) {
+      console.log('Game finished');
+      return;
+    }
   }
 
   if (coord.length == 0) {
