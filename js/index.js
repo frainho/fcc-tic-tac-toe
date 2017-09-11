2534fb539242e4ad5c162c7883c001a4b9427716
Filipe Rainho <frainho@Filipes-MacBook-Pro.local>
2017-09-11 23:08:46 +0200

    git add .

--- js/index.js
+++ js/index.js
@@ -1,18 +1,23 @@
 $( document ).ready(function() {
   var playerOps = ["O", "X"];
+  var coord = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   
+
   $('td').click( function(){
     $(this).text("X");
+    var idInTable = $(this).attr('id');
+    console.log(idInTable);
+    var removeNum = coord.indexOf(idInTable);
+    console.log(removeNum);
+    if (removeNum != -1) {
+      array.splice(removeNum, 1);
+    }
+    console.log(coord);
   }); 
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  function computerMove() {
+
+  }
+ 
   
 });
