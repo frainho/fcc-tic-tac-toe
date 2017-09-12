55c4f70d81691c61512eccc56a89bc53a04c1192
ncoder92 <filipenrainho@gmail.com>
2017-09-12 12:26:50 +0200

    symbol choice and randomization of first player

--- js/index.js
+++ js/index.js
@@ -2,29 +2,51 @@ $(document).ready(function () {
   var playerOps = ["O", "X"];
   var coord = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+//choice of which symbol the player wants to use
+var playerSymbol;
+var computerSymbol;
 
-  $('td').click(function () {
-    $(this).text("X");
-    let idInTable = $(this).attr('id');
-    let removeNum = coord.indexOf(idInTable);
-    console.log(removeNum);
-    if (removeNum != -1) {
-      coord.splice(removeNum, 1);
-    }
-    console.log(coord);
-    if (coord.length == 0) {
-      console.log('Game finished');
-      return;
-    }
+$("#btnX").click(function() {
+  playerSymbol = "X";
+  computerSymbol = "O";
+  whoGFirst();
+});
+
+$("#btnO").click(function() {
+  playerSymbol = "O";
+  computerSymbol = "X";
+  whoGFirst();
+});
+
+
+//randomization of which player starts
+function whoGFirst() {
+  var firstPlayer = Math.random();
+  if (firstPlayer < 0.5) {
     computerMove();
+  }
+}
+//listen for a click on a table cell and add the chosen symbol to it
+  $('td').click(function () {
+      $(this).text(playerSymbol);
+      var idInTable = $(this).attr('id');
+      var removeNum = coord.indexOf(idInTable);
+      if (removeNum != -1) {
+        coord.splice(removeNum, 1);
+      }
+      if (coord.length == 0) {
+        console.log('Game finished');
+        return;
+      }
+      computerMove();
   });
 
   function computerMove() {
-    let computerPlay = coord[Math.floor(Math.random() * coord.length)];
-    $('#' + computerPlay).text("O");
-    let idInTable = $('#' + computerPlay).attr('id');
-    let removeNum = coord.indexOf(idInTable);
-    console.log(removeNum);
+    console.log(computerSymbol);
+    var computerPlay = coord[Math.floor(Math.random() * coord.length)];
+    $('#' + computerPlay).text(computerSymbol);
+    var idInTable = $('#' + computerPlay).attr('id');
+    var removeNum = coord.indexOf(idInTable);
     if (removeNum != -1) {
       coord.splice(removeNum, 1);
     }
